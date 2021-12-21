2b6d609a65aa4e97e38b2713900277fe7b62b783
alxjrvs@gmail.com <alxjrvs@gmail.com>
2021-12-20 22:21:12 -0500

    Lil cleanups hither and yon

--- src/digestNotation/digestNotation.ts
+++ src/digestNotation/digestNotation.ts
@@ -1,17 +1,5 @@
-const corePattern = /(\d+)[dD](\d+)/g
+import { digestCore } from './digestCore'
 
 export function digestNotation(notationString: string) {
-  const coreMatches = notationString.toLowerCase().match(corePattern)
-
-  if (!coreMatches) {
-    throw `Dice Notation is not parseable. Received: ${notationString}`
-  }
-
-  const core = coreMatches[0]
-  const [rolls, sides] = core.split('d').map(Number)
-
-  return {
-    rolls,
-    sides,
-  }
+  return { ...digestCore(notationString) }
 }
