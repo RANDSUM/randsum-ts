971915fd9f1aaca761fce56e70223750fb7b02c4
alxjrvs@gmail.com <alxjrvs@gmail.com>
2021-12-22 01:40:50 -0500

    move DigestNotation matching around

--- src/digestNotation/digestNotation.ts
+++ src/digestNotation/digestNotation.ts
@@ -1,5 +1,12 @@
 import { digestCore } from './digestCore'
+import { diceNotationPattern } from './matchers'
 
 export function digestNotation(notationString: string) {
-  return { ...digestCore(notationString) }
+  const coreMatches = notationString.toLowerCase().match(diceNotationPattern)
+
+  if (!coreMatches) {
+    throw `Dice Notation is not parseable. Received: ${notationString}`
+  }
+
+  return { ...digestCore(coreMatches[0]) }
 }
