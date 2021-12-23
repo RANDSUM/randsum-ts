04f6a3f8a8ebceeafb5a5a97be2381528dccb7a5
alxjrvs@gmail.com <alxjrvs@gmail.com>
2021-12-23 00:54:53 -0500

    REduce

--- src/digestNotation/digestModifiers.ts
+++ src/digestNotation/digestModifiers.ts
@@ -1,5 +1,10 @@
 import { parseDropNotation } from './notationParsers'
 
 export function digestModifiers(modifierString: string) {
-  return { drop: parseDropNotation(modifierString) }
+  return [{ drop: parseDropNotation(modifierString) }].reduce((parameters, current) => {
+    if (Object.values(current)[0]) {
+      return { ...parameters, ...current }
+    }
+    return parameters
+  }, {})
 }
