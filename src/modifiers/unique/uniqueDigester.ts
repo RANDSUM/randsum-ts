dbefc91cb2e1e8985f29c2583fdcc57277eb15b7
alxjrvs@gmail.com <alxjrvs@gmail.com>
2021-12-23 08:49:17 -0500

    Fixed unique

--- src/modifiers/unique/uniqueDigester.ts
+++ src/modifiers/unique/uniqueDigester.ts
@@ -8,14 +8,14 @@ export function uniqueDigester(
   if (rolls > sides) {
     throw 'You cannot have unique rolls when there are more rolls than sides of die.'
   }
-  const notUnique = typeof unique === 'boolean' ? false : unique?.notUnique
+  const notUnique = !unique || typeof unique === 'boolean' ? [] : unique.notUnique
 
-  const filteredArray = rollTotals.filter(n => !(notUnique && notUnique.includes(n)))
+  const filteredArray = rollTotals.filter(n => !notUnique.includes(n))
   const fixedRollTotals = rollTotals.map((num, index, arr) => {
     let roll
     switch (true) {
       case arr.indexOf(num) === index:
-      case !!notUnique && notUnique.includes(num):
+      case notUnique.includes(num):
         return num
       default:
         do {
