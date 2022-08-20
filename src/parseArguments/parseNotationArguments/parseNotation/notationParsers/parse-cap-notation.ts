01a7a3bacff1adcca6e1cc32df5f887a5ac7c17f
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-08-19 20:20:00 -0400

    Try to use constrained regexp

--- src/parseArguments/parseNotationArguments/parseNotation/notationParsers/parse-cap-notation.ts
+++ src/parseArguments/parseNotationArguments/parseNotation/notationParsers/parse-cap-notation.ts
@@ -2,7 +2,7 @@ import { CapModifier } from 'types'
 
 export function parseCapNotation (notationString: string): CapModifier<number> {
   let capParameters = {}
-  const capString = notationString.split('c')[1].split(/(?!\d)/)
+  const capString = notationString.split('c')[1].split(/(?!\d{1,99})/)
   for (const note of capString) {
     if (note.includes('<')) {
       capParameters = { ...capParameters, lessThan: Number(note.replace('<', '')) }
