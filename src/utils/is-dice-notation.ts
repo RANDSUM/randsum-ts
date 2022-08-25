c0e626b2a17874a6b016330e16b6dcbe3527fa6d
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-08-25 18:40:21 -0400

    Condense the utils

--- src/utils/is-dice-notation.ts
+++
@@ -1,7 +0,0 @@
-import { DiceNotation } from 'types'
-
-import { coreNotationPattern } from './patterns'
-
-export function isDiceNotation(argument: unknown): argument is DiceNotation {
-  return !!coreNotationPattern.test(String(argument))
-}
--- src/utils/make-rolls.ts
+++
@@ -1,7 +0,0 @@
-export function makeRolls(quantity: number, rollOne: () => number) {
-  const rolls = new Array<number>(quantity)
-  for (let index = 0; index < quantity; index += 1) {
-    rolls[index] = rollOne()
-  }
-  return rolls
-}
