35c8b94cb6052acdc844337f8e103b194a317245
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-08-25 21:26:33 -0400

    fix lint-staged, format

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import { terser } from "rollup-plugin-terser"
-import pluginTypescript from "@rollup/plugin-typescript"
-import pluginCommonjs from "@rollup/plugin-commonjs"
-import pluginNodeResolve from "@rollup/plugin-node-resolve"
-import { babel } from "@rollup/plugin-babel"
-import * as path from "path"
-import pkg from "./package.json"
+import { terser } from 'rollup-plugin-terser'
+import pluginTypescript from '@rollup/plugin-typescript'
+import pluginCommonjs from '@rollup/plugin-commonjs'
+import pluginNodeResolve from '@rollup/plugin-node-resolve'
+import { babel } from '@rollup/plugin-babel'
+import * as path from 'path'
+import pkg from './package.json'
 
-const moduleName = pkg.name.replace(/^@.*\//, "")
-const inputFileName = "src/index.ts"
+const moduleName = pkg.name.replace(/^@.*\//, '')
+const inputFileName = 'src/index.ts'
 const author = pkg.author
 const banner = `
   /**
@@ -25,34 +25,34 @@ export default [
       {
         name: moduleName,
         file: pkg.browser,
-        format: "iife",
-        sourcemap: "inline",
-        banner,
+        format: 'iife',
+        sourcemap: 'inline',
+        banner
       },
       {
         name: moduleName,
-        file: pkg.browser.replace(".js", ".min.js"),
-        format: "iife",
-        sourcemap: "inline",
+        file: pkg.browser.replace('.js', '.min.js'),
+        format: 'iife',
+        sourcemap: 'inline',
         banner,
-        plugins: [terser()],
-      },
+        plugins: [terser()]
+      }
     ],
     plugins: [
       pluginTypescript({
         tsconfig: './tsconfig.build.json'
       }),
       pluginCommonjs({
-        extensions: [".js", ".ts"],
+        extensions: ['.js', '.ts']
       }),
       babel({
-        babelHelpers: "bundled",
-        configFile: path.resolve(__dirname, ".babelrc.js"),
+        babelHelpers: 'bundled',
+        configFile: path.resolve(__dirname, '.babelrc.js')
       }),
       pluginNodeResolve({
-        browser: true,
-      }),
-    ],
+        browser: true
+      })
+    ]
   },
 
   // ES
@@ -61,31 +61,31 @@ export default [
     output: [
       {
         file: pkg.module,
-        format: "es",
-        sourcemap: "inline",
+        format: 'es',
+        sourcemap: 'inline',
         banner,
-        exports: "named",
-      },
+        exports: 'named'
+      }
     ],
     external: [
       ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.devDependencies || {}),
+      ...Object.keys(pkg.devDependencies || {})
     ],
     plugins: [
       pluginTypescript({
         tsconfig: './tsconfig.build.json'
       }),
       pluginCommonjs({
-        extensions: [".js", ".ts"],
+        extensions: ['.js', '.ts']
       }),
       babel({
-        babelHelpers: "bundled",
-        configFile: path.resolve(__dirname, ".babelrc.js"),
+        babelHelpers: 'bundled',
+        configFile: path.resolve(__dirname, '.babelrc.js')
       }),
       pluginNodeResolve({
-        browser: false,
-      }),
-    ],
+        browser: false
+      })
+    ]
   },
 
   // CommonJS
@@ -94,30 +94,30 @@ export default [
     output: [
       {
         file: pkg.main,
-        format: "cjs",
-        sourcemap: "inline",
+        format: 'cjs',
+        sourcemap: 'inline',
         banner,
-        exports: "default",
-      },
+        exports: 'default'
+      }
     ],
     external: [
       ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.devDependencies || {}),
+      ...Object.keys(pkg.devDependencies || {})
     ],
     plugins: [
       pluginTypescript({
         tsconfig: './tsconfig.build.json'
       }),
       pluginCommonjs({
-        extensions: [".js", ".ts"],
+        extensions: ['.js', '.ts']
       }),
       babel({
-        babelHelpers: "bundled",
-        configFile: path.resolve(__dirname, ".babelrc.js"),
+        babelHelpers: 'bundled',
+        configFile: path.resolve(__dirname, '.babelrc.js')
       }),
       pluginNodeResolve({
-        browser: false,
-      }),
-    ],
-  },
+        browser: false
+      })
+    ]
+  }
 ]
