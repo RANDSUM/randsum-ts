f1a6f4bd332c83ce92dd05c1c9121e2151211c19
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-08-27 10:00:46 -0400

    make randsum args complicated juuuust in case

--- src/types/arguments.ts
+++ src/types/arguments.ts
@@ -1,16 +1,17 @@
 import { RandsumOptions, SecondaryRandsumOptions, UserOptions } from './options'
 import { DieType, DetailedType, DiceNotation, NumberString } from './primitives'
 
-export type RandsumArguments = {
-  primeArgument:
-    | RandsumOptions<DieType, DetailedType>
-    | DiceNotation<DieType>
-    | NumberString
-  secondArgument?:
-    | SecondaryRandsumOptions<DieType, DetailedType>
-    | UserOptions<DetailedType>
+export type RandsumArguments<
+  N extends DieType = DieType,
+  D extends DetailedType = DetailedType
+> = {
+  primeArgument: RandsumOptions<N, D> | DiceNotation<N> | NumberString
+  secondArgument?: SecondaryRandsumOptions<N, D> | UserOptions<D>
 }
 
-export type AllArgumentTypes =
-  | RandsumArguments['primeArgument']
-  | RandsumArguments['secondArgument']
+export type AllArgumentTypes<
+  N extends DieType = DieType,
+  D extends DetailedType = DetailedType
+> =
+  | RandsumArguments<N, D>['primeArgument']
+  | RandsumArguments<N, D>['secondArgument']
