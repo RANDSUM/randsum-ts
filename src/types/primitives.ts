d38127f31b31188b66b511ef6255544b215c7bb4
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-08-28 17:36:02 -0400

    Tweak

--- src/types/primitives.ts
+++ src/types/primitives.ts
@@ -15,10 +15,9 @@ export type DiceNotation<N extends DieType = DieType> = N extends StandardDie
   ? DiceNotationWithNumericSides
   : DiceNotationWithCustomSides
 
-export type NumberStringArgument = number | 'inclusive'
-
 export type TypeOrArrayOfType<T> = T | T[]
 
+export type NumberStringArgument = number | 'inclusive'
 export type NumberString<T extends NumberStringArgument = 'inclusive'> =
   T extends 'inclusive' ? number | `${number}` : number
 
