d44db71ec21120f710e48df9b08c96b1cf5639aa
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-10-07 22:59:25 -0400

    Fix utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-import { allPatterns, coreNotationPattern } from 'patterns'
-=======
 import { allPatterns, coreNotationPattern } from './patterns'
->>>>>>> 44abf35 (Wip)
 import {
   Match,
   Randomizer,
   DiceNotation,
   RandsumOptions,
   DieType,
-<<<<<<< HEAD
-  DetailedType
-} from 'types'
-=======
   DetailedType,
 } from './types'
->>>>>>> 44abf35 (Wip)
 
 export const completeRollPattern = new RegExp(
   `${allPatterns.map((pattern) => pattern.source).join('|')}`,
@@ -36,13 +27,8 @@ export function findMatches(notations: string): Match[] {
               [key]:
                 key === 'coreNotationMatch'
                   ? value
-<<<<<<< HEAD
-                  : value.toLowerCase().replace(/s+/, '')
-            }
-=======
                   : value.toLowerCase().replace(/s+/, ''),
             },
->>>>>>> 44abf35 (Wip)
           ]
         }
       }
@@ -79,6 +65,6 @@ export function isRandsumOptions(
   return (
     typeof argument === 'object' &&
     typeof (argument as RandsumOptions<DieType, DetailedType>).sides !==
-      undefined
+    undefined
   )
 }
