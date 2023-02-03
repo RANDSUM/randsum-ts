3f688b8c1d4e642cf308ecd84bd332f618cd4b35
alxjrvs@gmail.com <alxjrvs@gmail.com>
2023-02-02 23:57:45 -0500

    Lint

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,12 @@
 /* eslint-disable unicorn/prefer-module */
 module.exports = {
-  "coverageDirectory": "../coverage/",
-  "collectCoverage": true,
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
+  coverageDirectory: '../coverage/',
+  collectCoverage: true,
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
   },
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$",
-  "moduleDirectories": [
-    "node_modules",
-    "src"
-  ],
-  "moduleFileExtensions": [
-    "ts",
-    "js"
-  ]
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'js']
 }
 /* eslint-enable unicorn/prefer-module */
