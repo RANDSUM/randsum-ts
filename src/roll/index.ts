933e9830997c56cd816064ddb2487d3ce7d71157
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-05-01 17:22:49 -0400

    Parse Args, moving on to other tests

--- src/roll/index.ts
+++ src/roll/index.ts
@@ -1,6 +1,6 @@
 import { RollArgument } from '../types/argument'
 import { RollResult } from '../types/results'
-import generateResult from './generate-results'
+import generateResult from './generate-result'
 import parseArguments from './parse-arguments'
 
 export default function roll(arg?: RollArgument): RollResult {
--- tests/generate-result.test.ts
+++ tests/generate-result.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, test } from 'bun:test'
 
-import { dicePoolFactory, StandardDie } from '../src/Die'
-import generateResult from '../src/roll/generate-results'
-import { InvalidUniqueError } from '../src/roll/generate-results/apply-modifiers'
+import { StandardDie } from '../src/Die'
+import generateResult from '../src/roll/generate-result'
+import { InvalidUniqueError } from '../src/roll/generate-result/apply-modifiers'
 import { Modifiers } from '../src/types/options'
 import { RollParameters } from '../src/types/parameters'
 
