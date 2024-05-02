7428d63f8400a813333f03c2ad7c9c724db04fca
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-05-02 07:25:07 -0400

    Raw Roll test

---
+++ tests/generate-raw-rolls.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, test } from 'bun:test'
+
+import { DicePoolParameters, RollParameters } from '../src'
+import generateRawRolls from '../src/roll/generate-result/generate-raw-rolls'
+
+describe('generateRawRolls', () => {
+  describe('when given a RollParameters object', () => {
+    const dicePoolParams: RollParameters = {
+      dicePools: {
+        'first-id': {
+          argument: 20,
+          options: {
+            quantity: 4,
+            sides: 20
+          },
+          die: {
+            roll: () => 1
+          } as unknown as DicePoolParameters<number>['die']
+        },
+        'second-id': {
+          argument: ['h', 't'],
+          options: {
+            quantity: 1,
+            sides: ['h', 't']
+          },
+          die: {
+            roll: () => 'h'
+          } as unknown as DicePoolParameters<string>['die']
+        }
+      }
+    }
+
+    test('returns an object with the same keys and an array of rolls equal to the desired quality with the provided die', () => {
+      const result = generateRawRolls(dicePoolParams.dicePools)
+
+      expect(result).toMatchObject({
+        'first-id': [1, 1, 1, 1],
+        'second-id': ['h']
+      })
+    })
+  })
+})
