a4d292a85122aaeecc2ce3d4e7562391af3cc188
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-05-04 08:49:20 -0400

    Export roll args

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export * from './Die'
 export { default as roll } from './roll'
-export { default as parseRollArguments } from './roll/parse-roll-arguments'
+export {
+  default as parseRollArguments,
+  parseRollArgument
+} from './roll/parse-roll-arguments'
 export { default as generateRollResult } from './roll/generate-roll-result'
 export * from './types'
