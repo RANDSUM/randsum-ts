cd3a5bf2bb60868fd1d336728b3b5ed7f9c85550
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-07-07 22:58:56 -0400

    More tweak

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const coreNotationPattern = /(?<coreNotationMatch>^\d+[Dd](\d+|{.*}))/
 export const modifierRollPatterns =
-  /(?<dropHighMatch>[Hh]\d*)|(?<dropLowMatch>[Ll]\d*)|(?<dropConstraintsMatch>[Dd]{?([<>|]?\d+,?)*}+)|(?<explodeMatch>!+{?([<>|]?\d+,?)*}?)|(?<uniqueMatch>[Uu]({(\d+,?)+})?)|(?<replaceMatch>[Vv]{?([<>|]?\d+=?\d+,?)*}?)|(?<rerollMatch>[Rr]{?([<>|]?\d,?)*}\d*)|(?<capMatch>[Cc]([<>|]?\d+)*)|(?<plusMatch>\+\d+)|(?<minusMatch>-\d+)/
+  /(?<dropHighMatch>[Hh]\d*)|(?<dropLowMatch>[Ll]\d*)|(?<dropConstraintsMatch>[Dd]{([<>|]?\d+,?)*})|(?<explodeMatch>!+{?([<>|]?\d+,?)*}?)|(?<uniqueMatch>[Uu]({(\d+,?)+})?)|(?<replaceMatch>[Vv]{?([<>|]?\d+=?\d+,?)*}?)|(?<rerollMatch>[Rr]{?([<>|]?\d,?)*}\d*)|(?<capMatch>[Cc]([<>|]?\d+)*)|(?<plusMatch>\+\d+)|(?<minusMatch>-\d+)/
 
 export const completeRollPattern = new RegExp(
   `${coreNotationPattern.source}|${modifierRollPatterns.source}`,
