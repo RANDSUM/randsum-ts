3967a1f6ad5f29ae2ff504d8ca1d044f6560c41b
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-09-07 12:18:00 -0400

    More possible

--- src/matchPattern.ts
+++ src/matchPattern.ts
@@ -2,28 +2,29 @@ export const coreNotationPattern = /^\d+[Dd](\d+|{.*})/
 export const dropHighestPattern = /[Hh]\d*/g
 export const dropLowestPattern = /[Ll]\d*/g
 export const uniquePattern = /[Uu]({(\d+,)*(\d+)})?/g
-export const replacePattern = /[Vv]{([<>]?\d+=?\d+,)*([<>]?\d+=?\d+)}/g
-
-const greaterThanLessThanMatcher = /{([<>]?\d+,)*([<>]?\d+)}/
-
-const coreDropConstraints = /[Dd]/
-export const dropConstraintsPattern = new RegExp(
-  coreDropConstraints.source + greaterThanLessThanMatcher.source,
-  'g'
-)
-
-const coreReroll = /[Rr]/
-export const rerollPattern = new RegExp(
-  coreReroll.source + greaterThanLessThanMatcher.source + /\d*/.source,
-  'g'
-)
-
-const coreCap = /[Cc]/
-export const capPattern = new RegExp(
-  coreCap.source + greaterThanLessThanMatcher.source,
-  'g'
-)
-
 export const plusPattern = /\+\d+/g
 export const minusPattern = /-\d+/g
 export const explodePattern = /!/g
+
+const greaterThanLessThanMatcher = /{([<>]?\d+,)*([<>]?\d+)}/
+const greaterThanLessEqualityThanMatcher = /{([<>]?\d+=?\d+,)*([<>]?\d+=?\d+)}/
+
+export const replacePattern = new RegExp(
+  /[Vv]/.source + greaterThanLessEqualityThanMatcher.source,
+  'g'
+)
+
+export const dropConstraintsPattern = new RegExp(
+  /[Dd]/.source + greaterThanLessThanMatcher.source,
+  'g'
+)
+
+export const rerollPattern = new RegExp(
+  /[Rr]/.source + greaterThanLessThanMatcher.source + /\d*/.source,
+  'g'
+)
+
+export const capPattern = new RegExp(
+  /[Cc]/.source + greaterThanLessThanMatcher.source,
+  'g'
+)
