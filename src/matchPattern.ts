73819473b0544c0b52926e3e08595df2edfeb1ce
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-09-07 12:16:12 -0400

    Combine where possible

--- src/matchPattern.ts
+++ src/matchPattern.ts
@@ -1,11 +1,29 @@
 export const coreNotationPattern = /^\d+[Dd](\d+|{.*})/
 export const dropHighestPattern = /[Hh]\d*/g
 export const dropLowestPattern = /[Ll]\d*/g
-export const dropConstraintsPattern = /[Dd]{([<>]?\d+,)*([<>]?\d+)}/g
-export const explodePattern = /!/g
 export const uniquePattern = /[Uu]({(\d+,)*(\d+)})?/g
 export const replacePattern = /[Vv]{([<>]?\d+=?\d+,)*([<>]?\d+=?\d+)}/g
-export const rerollPattern = /[Rr]{([<>]?\d+,)*([<>]?\d+)}\d*/g
-export const capPattern = /[Cc]{([<>]?\d+,)*([<>]?\d+)}/g
+
+const greaterThanLessThanMatcher = /{([<>]?\d+,)*([<>]?\d+)}/
+
+const coreDropConstraints = /[Dd]/
+export const dropConstraintsPattern = new RegExp(
+  coreDropConstraints.source + greaterThanLessThanMatcher.source,
+  'g'
+)
+
+const coreReroll = /[Rr]/
+export const rerollPattern = new RegExp(
+  coreReroll.source + greaterThanLessThanMatcher.source + /\d*/.source,
+  'g'
+)
+
+const coreCap = /[Cc]/
+export const capPattern = new RegExp(
+  coreCap.source + greaterThanLessThanMatcher.source,
+  'g'
+)
+
 export const plusPattern = /\+\d+/g
 export const minusPattern = /-\d+/g
+export const explodePattern = /!/g
