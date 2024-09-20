b748c3fdaf98d6c057e4e3ad9d8be4b7f22a340b
Alex Jarvis <alxjrvs@users.noreply.github.com>
2024-09-20 00:35:49 -0400

    Mark Dice as Custom or Standard  (#630)

--- src/D.ts
+++ src/D.ts
@@ -1,19 +1,24 @@
 import { isCustomSides } from '~guards'
+import { DicePoolType } from '~types'
 
+type Type<T> = T extends string[] ? DicePoolType.custom : DicePoolType.standard
 type Faces<T> = T extends string[] ? T : number[]
-type Result<T> = T extends number[] ? number : string
+type Result<F> = F extends number[] ? number : string
 
 class D<Sides extends string[] | number> {
   sides: number
   faces: Faces<Sides>
+  type: Type<Sides>
 
   constructor(sides: Sides) {
     if (isCustomSides(sides)) {
       this.sides = sides.length
+      this.type = DicePoolType.custom as Type<Sides>
       this.faces = sides as Faces<Sides>
       return
     }
     this.sides = sides
+    this.type = DicePoolType.standard as Type<Sides>
     this.faces = Array.from(
       { length: Number(sides) },
       (_, index) => index + 1
