bebcd44c019add1e7be32029567c50f7dd5e3047
alxjrvs@gmail.com <alxjrvs@gmail.com>
2024-09-24 10:05:05 -0400

    Tweak Fudge dice

--- src/D.ts
+++ src/D.ts
@@ -49,13 +49,4 @@ class D<Sides extends string[] | number> {
   }
 }
 
-const D4 = new D(4)
-const D6 = new D(6)
-const D8 = new D(8)
-const D10 = new D(10)
-const D12 = new D(12)
-const D20 = new D(20)
-const D100 = new D(100)
-const FudgeDice = new D(['+', '+', '+', '-', '0', '0'])
-
-export { D, D4, D6, D8, D10, D12, D20, D100, FudgeDice }
+export { D }
