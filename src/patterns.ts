fcf40fe55057dce55e032cdb20002f82f5a79f42
Alex Jarvis <alxjrvs@users.noreply.github.com>
2024-11-01 22:14:59 -0400

    Mild tweak to patterns (#641)

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,3 +1,5 @@
+const digitPattern = /\d*/.source
+
 export const coreNotationPattern = /^\d+[Dd](\d+|{.*})/
 export const dropHighestPattern = /[Hh]\d*/g
 export const dropLowestPattern = /[Ll]\d*/g
@@ -11,33 +13,47 @@ const coreGreaterLessThanEquals = new RegExp(
   coreGreaterLessThan.source + /=?\d+/.source
 )
 
-function oneOrMany(core: RegExp) {
+function createRepeatedPattern(core: RegExp) {
   return new RegExp(`{(${core.source},)*(${core.source})}`, 'g')
 }
 
-const greaterThanLessEqualityThanMatcher = oneOrMany(coreGreaterLessThanEquals)
+const greaterThanLessEqualityThanMatcher = createRepeatedPattern(
+  coreGreaterLessThanEquals
+)
 export const replacePattern = new RegExp(
   /[Vv]/.source + greaterThanLessEqualityThanMatcher.source,
   'g'
 )
 
-const greaterThanLessThanMatcher = oneOrMany(coreGreaterLessThan)
+const greaterThanLessThanMatcher = createRepeatedPattern(coreGreaterLessThan)
 export const dropConstraintsPattern = new RegExp(
   /[Dd]/.source + greaterThanLessThanMatcher.source,
   'g'
 )
 
 export const rerollPattern = new RegExp(
-  /[Rr]/.source + greaterThanLessThanMatcher.source + /\d*/.source,
+  `${/[Rr]/.source}${greaterThanLessThanMatcher.source}${digitPattern}`,
   'g'
 )
 
 export const capPattern = new RegExp(
-  /[Cc]/.source + greaterThanLessThanMatcher.source,
+  `${/[Cc]/.source}${greaterThanLessThanMatcher.source}`,
   'g'
 )
 
 export const completeRollPattern = new RegExp(
-  `${coreNotationPattern.source}|${dropHighestPattern.source}|${dropLowestPattern.source}|${dropConstraintsPattern.source}|${explodePattern.source}|${uniquePattern.source}|${replacePattern.source}|${rerollPattern.source}|${capPattern.source}|${plusPattern.source}|${minusPattern.source}`,
+  [
+    coreNotationPattern.source,
+    dropHighestPattern.source,
+    dropLowestPattern.source,
+    dropConstraintsPattern.source,
+    explodePattern.source,
+    uniquePattern.source,
+    replacePattern.source,
+    rerollPattern.source,
+    capPattern.source,
+    plusPattern.source,
+    minusPattern.source
+  ].join('|'),
   'g'
 )
