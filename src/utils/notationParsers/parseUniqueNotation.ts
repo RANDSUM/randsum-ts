5def28f314d3789f5677e604782d1019ddf8c9a0
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-03-14 09:01:49 -0400

    Cleanup parse unique

--- src/utils/notationParsers/parseUniqueNotation.ts
+++ src/utils/notationParsers/parseUniqueNotation.ts
@@ -5,29 +5,24 @@ import { extractMatches } from './extractMatches'
 export function parseUniqueNotation(
   modifiersString: string
 ): Pick<Modifiers, 'unique'> {
-  const notations = extractMatches(modifiersString, uniquePattern)
-  if (notations.length === 0) {
-    return {}
-  }
-  return notations.reduce(
-    (acc, notationString) => {
-      if (notationString === 'u' || notationString === 'U') {
-        if (typeof acc.unique === 'object') {
-          return acc
-        }
-        return { unique: true }
+  return extractMatches(modifiersString, uniquePattern).reduce<
+    Pick<Modifiers, 'unique'>
+  >((acc, notationString) => {
+    if (notationString.toUpperCase() === 'U') {
+      if (typeof acc.unique === 'object') {
+        return acc
       }
-      const notUnique = notationString
-        .replaceAll(/[Uu]{/g, '')
-        .replaceAll('}', '')
-        .split(',')
+      return { unique: true }
+    }
+    const notUnique = notationString
+      .replaceAll(/[Uu]{/g, '')
+      .replaceAll('}', '')
+      .split(',')
 
-      return {
-        unique: {
-          notUnique: notUnique.map(Number)
-        }
+    return {
+      unique: {
+        notUnique: notUnique.map(Number)
       }
-    },
-    { unique: false } as Pick<Modifiers, 'unique'>
-  )
+    }
+  }, {})
 }
