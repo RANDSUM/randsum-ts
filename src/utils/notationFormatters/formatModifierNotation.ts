32a841cab51b1b115e7fbdea6b78472723b37692
alxjrvs@gmail.com <alxjrvs@gmail.com>
2025-03-23 15:58:40 -0400

    WIP

--- src/utils/notationFormatters/formatModifierNotation.ts
+++ src/utils/notationFormatters/formatModifierNotation.ts
@@ -1,13 +1,13 @@
 import { isValidModifier } from '~src/guards/isValidModifier'
 import { ExplodeModifier } from '~src/modifiers/ExplodeModifier'
+import { MinusModifier } from '~src/modifiers/MinusModifier'
 import { PlusModifier } from '~src/modifiers/PlusModifier'
+import { RerollModifier } from '~src/modifiers/RerollModifier'
 import { UniqueModifier } from '~src/modifiers/UniqueModifier'
 import type { RollOptions } from '~types'
 import { capNotation } from './capNotation'
 import { dropNotation } from './dropNotation'
-import { minusNotation } from './minusNotation'
 import { replaceNotation } from './replaceNotation'
-import { rerollNotation } from './rerollNotation'
 
 export function formatModifierNotation({ modifiers }: RollOptions): string {
   if (!isValidModifier(modifiers)) return ''
@@ -16,11 +16,11 @@ export function formatModifierNotation({ modifiers }: RollOptions): string {
     modifiers.cap && capNotation(modifiers.cap),
     modifiers.drop && dropNotation(modifiers.drop),
     modifiers.replace && replaceNotation(modifiers.replace),
-    modifiers.reroll && rerollNotation(modifiers.reroll),
+    modifiers.reroll && new RerollModifier(modifiers.reroll).toNotation(),
     modifiers.explode && new ExplodeModifier(modifiers.explode).toNotation(),
     modifiers.unique && new UniqueModifier(modifiers.unique).toNotation(),
     modifiers.plus && new PlusModifier(modifiers.plus).toNotation(),
-    modifiers.minus && minusNotation(modifiers.minus)
+    modifiers.minus && new MinusModifier(modifiers.minus).toNotation()
   ]
     .flat()
     .filter((i) => typeof i === 'string')
